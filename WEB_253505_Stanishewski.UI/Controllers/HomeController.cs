6c6055ac8e95617da49bfc8b8520835026b253a1
Ga1ax9y <sasha20050803@gmail.com>
2024-09-08 22:32:37 +0300

    Добавьте файлы проекта.

---
+++ WEB_253505_Stanishewski.UI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_253505_Stanishewski.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WEB_253505_Stanishewski.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_253505_Stanishewski.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
