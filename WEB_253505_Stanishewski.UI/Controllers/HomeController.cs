67e4f1355ec11ade9228f2bb16b72948d2c75e93
Ga1ax9y <sasha20050803@gmail.com>
2024-09-17 17:55:38 +0300

    Second lab added

--- WEB_253505_Stanishewski.UI/Controllers/HomeController.cs
+++ WEB_253505_Stanishewski.UI/Controllers/HomeController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WEB_253505_Stanishewski.UI.Models;
 
 namespace WEB_253505_Stanishewski.UI.Controllers
 {
     public class HomeController : Controller
     {
+        [ViewData]
+        public string Name { get; set; }
         public IActionResult Index()
         {
-            return View();
+            var items = new List<ListDemo>
+            {
+                new ListDemo { Id=1, Name="Item 1"},
+                new ListDemo { Id=2, Name="Item 2"},
+                new ListDemo { Id=3, Name="Item 3"}
+            };
+            var selectList = new SelectList(items, "Id", "Name");
+            Name = "Лабораторная работа 2";
+            return View(selectList); ;
         }
+
     }
 }
---
+++ WEB_253505_Stanishewski.UI/Models/ListDemo.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_253505_Stanishewski.UI.Models
+{
+    public class ListDemo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
